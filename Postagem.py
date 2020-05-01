0f43a9c1e2991af724be6152146ba9aaf5fadd4b
Leandro Fratel de Souza <leandrofratel@gmail.com>
2020-04-30 21:49:51 -0300

    Atualizado

--- Postagem.py
+++ Postagem.py
@@ -3,12 +3,11 @@ import json
 
 def postar():
     url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=31d49c4bec438e1e41cc7eec111fb4153ae67647'
-    files = {'answer': open('answer.json', 'rb')}
+    file = {'answer': open("answer.json", 'rb')}
+    response = requests.post(url, files=file)
 
-    r = requests.post(url, files=files)
-    r.text
-
-    print(r.status_code)
+    print(response.status_code)
+    print(response.text)
 
     
 postar()
